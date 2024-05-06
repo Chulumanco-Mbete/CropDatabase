d9355ad67f5003031eff7609bb31a5e8f9c9cf75
Chulumanco-Mbete <5100151@mypillar7uwc.ac.za>
2024-05-06 11:40:33 +0200

    Add project files.

---
+++ CropDatabase/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace CropDatabase
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ CropDatabase/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace CropDatabase
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
